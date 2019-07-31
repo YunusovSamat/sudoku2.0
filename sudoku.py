937ce7806695a334bb29bdc73880e6ff77876d15
Yunusov Samat <samat.yunusov@bk.ru>
2019-07-31 22:53:29 +0300

    Created class WorkSudoku

--- sudoku.py
+++ sudoku.py
@@ -1,33 +1,84 @@
 import numpy as np
 
 
-def sudoku_print(table):
-    for i in range(9):
-        print('|', end='')
-        for j in range(9):
-            print(table[i][j], '|' if j % 3 == 2 else '', sep='', end='')
-        print('\n', '-'*13+'\n' if i % 3 == 2 else '', sep='', end='')
+class WorkSudoku:
+    def __init__(self):
+        self.mtx = []
+
+    def str_to_mtx(self):
+        mtx_str = """\
+        ***6**1*7
+        68*9513**
+        **3**2568
+        *4*81**2*
+        ******85*
+        *9**65*73
+        4*9**3*85
+        162**9*3*
+        5**7*6***\
+        """
+        mtx_str = mtx_str.replace(' ', '')
+        mtx_str = mtx_str.split("\n")
+
+        for row in mtx_str:
+            for elem in row:
+                if elem == '*':
+                    self.mtx.append(list(range(1, 10)))
+                else:
+                    self.mtx.append(int(elem))
+        self.mtx = np.array(self.mtx).reshape((9, 9))
+
+    def print_sudoku(self):
+        print('-' * 36)
+        for vector in self.mtx:
+            for i in range(3):
+                for elem in vector:
+                    if isinstance(elem, list):
+                        print(elem[0 + i * 3], elem[1 + i * 3], elem[2 + i * 3], '|', sep='', end='')
+                    else:
+                        if i % 3 == 1:
+                            print(' ', elem, ' |', sep='', end='')
+                        else:
+                            print('   |', sep='', end='')
+                print()
+            print('-' * 36)
+        print()
+
+    def get_row_set(self, i):
+        set_temp = set()
+        for elem in self.mtx[i]:
+            if isinstance(elem, int):
+                set_temp.add(elem)
+        return set_temp
+
+    def get_col_set(self, j):
+        set_temp = set()
+        for elem in self.mtx[:, j]:
+            if isinstance(elem, int):
+                set_temp.add(elem)
+        return set_temp
+
+    def get_box_set(self, i, j):
+        set_temp = set()
+        row_bgn = i // 3 * 3
+        row_end = (i // 3 + 1) * 3
+        col_bgn = j // 3 * 3
+        col_end = (j // 3 + 1) * 3
+        for elem in self.mtx[row_bgn: row_end, col_bgn: col_end].ravel():
+            if isinstance(elem, int):
+                set_temp.add(elem)
+        return set_temp
+
+    def get_all_set(self, i, j):
+        return (set(range(1, 10)) - (self.get_row_set(i) |
+                self.get_col_set(j) | self.get_box_set(i, j)))
 
 
-tab_str = """\
-***6**1*7
-68*9513**
-**3**2568
-*4*81**2*
-******85*
-*9**65*73
-4*9**3*85
-162**9*3*
-5**7*6***\
-"""
-tab_str = tab_str.replace('*', '0')
-tab_str = tab_str.split("\n")
-tab = []
-for row in tab_str:
-    tab.append(list(map(int, row)))
+if __name__ == '__main__':
+    sudoku = WorkSudoku()
+    sudoku.str_to_mtx()
+    # sudoku.print_sudoku()
+    print(sudoku.get_all_set(0, 8))
 
-tab = np.array(tab, "int8")
-
-sudoku_print(tab)
 
 
